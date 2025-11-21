ec1a82cd81a51172814ced356d30262def217356
Ghulam Dastgir <ghulamdastgir12905@gmail.com>
2025-11-21 21:26:43 +0500

    TA file

--- file_manager/Input.cpp
+++ file_manager/Input.cpp
@@ -1,86 +1,94 @@
 #include <iostream>
 #include <fstream>
-#include <cstring>
+#include <vector>
 using namespace std;
 
-// Structs
-struct Building {
-    int buildingID;
-    char name[100];
-    int attendantID;
-};
+class TA {
+public:
+    int TAID;
+    string Name;
+    vector<int> AssignedLabSectionIDs;
 
-struct Room {
-    int roomID;
-    char roomNo[50];
-    int capacity;
-    int buildingID;
-};
+    void write(fstream &file) {
+        file.write((char*)&TAID, sizeof(TAID));
 
-struct Officer {
-    int officerID;
-    char name[100];
-    int managedBuildingID;
-};
+        int len = Name.size();
+        file.write((char*)&len, sizeof(len));
+        file.write(Name.c_str(), len);
 
-struct Attendant {
-    int attendantID;
-    char name[100];
-    int buildingID;
+        int count = AssignedLabSectionIDs.size();
+        file.write((char*)&count, sizeof(count));
+        for (int id : AssignedLabSectionIDs)
+            file.write((char*)&id, sizeof(id));
+    }
+
+    void read(fstream &file) {
+        file.read((char*)&TAID, sizeof(TAID));
+
+        int len;
+        file.read((char*)&len, sizeof(len));
+        Name.resize(len);
+        file.read(&Name[0], len);
+
+        int count;
+        file.read((char*)&count, sizeof(count));
+        AssignedLabSectionIDs.resize(count);
+        for (int i = 0; i < count; i++)
+            file.read((char*)&AssignedLabSectionIDs[i], sizeof(int));
+    }
+
+    void print() {
+        cout << TAID << " , " << Name << " , [";
+        for (int i = 0; i < AssignedLabSectionIDs.size(); i++) {
+            cout << AssignedLabSectionIDs[i];
+            if (i + 1 < AssignedLabSectionIDs.size()) cout << ",";
+        }
+        cout << "]\n";
+    }
 };
 
 int main() {
-    ofstream outText("all_data.txt");
-    if(!outText) {
-        cout << "Error creating all_data.txt" << endl;
-        return 1;
+    vector<TA> list = {
+        {4001, "Alex Martin", {2001,2002}},
+        {4002, "Brian Lopez", {2003}},
+        {4003, "Chloe Kim", {2004}},
+        {4004, "David Green", {2005}},
+        {4005, "Ella Foster", {2006}},
+        {4006, "Frank Turner", {2007}},
+        {4007, "Grace Lee", {2008}},
+        {4008, "Harry Patel", {2009,2010}},
+        {4009, "Rayan Ahmed", {3001,3002}},
+        {4010, "Liam Hughes", {4001,4002}},
+        {4011, "Dawood Shahzad", {5001,5002,5003}},
+        {4012, "Quinn Delgado", {6001}},
+        {4013, "Ruby Chen", {6002}},
+        {4014, "Steven Young", {7001}},
+        {4015, "Tara Mitchell", {7002}}
+    };
+
+    fstream file("../data/ta.dat", ios::out | ios::binary);
+    for (auto &t : list) t.write(file);
+    file.close();
+
+    fstream in("../data/ta.dat", ios::in | ios::binary);
+
+    while (true) {
+        TA t;
+        if (!in.read((char*)&t.TAID, sizeof(t.TAID))) break;
+
+        int len;
+        in.read((char*)&len, sizeof(len));
+        t.Name.resize(len);
+        in.read(&t.Name[0], len);
+
+        int count;
+        in.read((char*)&count, sizeof(count));
+        t.AssignedLabSectionIDs.resize(count);
+        for (int i = 0; i < count; i++)
+            in.read((char*)&t.AssignedLabSectionIDs[i], sizeof(int));
+
+        t.print();
     }
 
-    // --- Buildings ---
-    ifstream inBuildings("../data/buildings.dat", ios::binary);
-    if(inBuildings) {
-        Building b[10];
-        inBuildings.read((char*)b, sizeof(b));
-        outText << "=== Buildings ===\n";
-        for(int i=0;i<10;i++)
-            outText << b[i].buildingID << ", " << b[i].name << ", " << b[i].attendantID << "\n";
-        inBuildings.close();
-    }
-
-    // --- Rooms ---
-    ifstream inRooms("../data/rooms.dat", ios::binary);
-    if(inRooms) {
-        Room r[50];
-        inRooms.read((char*)r, sizeof(r));
-        outText << "\n=== Rooms ===\n";
-        for(int i=0;i<50;i++)
-            outText << r[i].roomID << ", " << r[i].roomNo << ", " << r[i].capacity << ", " << r[i].buildingID << "\n";
-        inRooms.close();
-    }
-
-    // --- Officers ---
-    ifstream inOfficers("../data/officers.dat", ios::binary);
-    if(inOfficers) {
-        Officer o[10];
-        inOfficers.read((char*)o, sizeof(o));
-        outText << "\n=== Officers ===\n";
-        for(int i=0;i<10;i++)
-            outText << o[i].officerID << ", " << o[i].name << ", " << o[i].managedBuildingID << "\n";
-        inOfficers.close();
-    }
-
-    // --- Attendants ---
-    ifstream inAttendants("../data/attendants.dat", ios::binary);
-    if(inAttendants) {
-        Attendant a[10];
-        inAttendants.read((char*)a, sizeof(a));
-        outText << "\n=== Attendants ===\n";
-        for(int i=0;i<10;i++)
-            outText << a[i].attendantID << ", " << a[i].name << ", " << a[i].buildingID << "\n";
-        inAttendants.close();
-    }
-
-    outText.close();
-    cout << "All data written to all_data.txt successfully!" << endl;
-    return 0;
+    in.close();
 }
